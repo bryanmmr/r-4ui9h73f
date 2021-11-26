1fcb6d3560a6eba94192f7ec1efbe898c57ca7a6
bryanmmr <bryanmmr@gmail.com>
2021-11-25 22:05:41 -0500

    add: repeater

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { throws } from 'assert';
 import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = { repeater : '' }
+  handleRepeater = (event) => {
+    this.setState({repeater : event.target.value})
+  }
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.handleRepeater} />
+        <p className="repeater">{this.state.repeater}</p>
       </div>
     );
   }
